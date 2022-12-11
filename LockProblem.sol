7c7137232e1f301736d161d131d8862094d83f7f
kimjuseong <k021229@naver.com>
2022-12-11 23:09:17 +0900

    자물쇠 풀기 문제

---
+++ LockProblem.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+contract LockProblem {
+    bool public lock = true;
+    address private player;
+
+    constructor(address _player) {
+        player = _player;
+    }
+
+    function unlock() public {
+        require(tx.origin == player, "Only the player(tx.origin) can unlock the lock");
+        require(msg.sender != player, "The player(msg.sender) cannot unlock the lock");
+        lock = false;
+    }
+}
+
+contract Key {
+    LockProblem public lockProblem;
+
+    constructor(address _problemAddress) {
+        lockProblem = LockProblem(_problemAddress);
+    }
+
+    function unlock() public{
+        lockProblem.unlock();
+    }
+}

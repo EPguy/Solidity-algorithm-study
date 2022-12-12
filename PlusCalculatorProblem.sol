d500674876044961202be23e3920f3b83dd7f33e
kjs <k021229@naver.com>
2022-12-12 09:29:17 +0900

    덧셈 문제

---
+++ PlusCalculatorProblem.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IPlusCalculator {
+    function plus(uint256, uint256) external pure returns (uint256);
+}
+
+contract PlusCalculatorProblem{
+    IPlusCalculator public plusCalculator;
+
+    function setPlusCalculator(address _plusCalculator) public {
+        plusCalculator = IPlusCalculator(_plusCalculator);
+    }
+}
+
+contract MyPlusCalculator is IPlusCalculator {
+    PlusCalculatorProblem public plusCalculatorProblem;
+
+    constructor(address _problemAddress) {
+        plusCalculatorProblem = PlusCalculatorProblem(_problemAddress);
+    }
+
+    function setCalculator() public{
+        plusCalculatorProblem.setPlusCalculator(address(this));
+    }
+
+    function plus(uint256 input1, uint256 input2) override public pure returns (uint256){
+        return input1 + input2;
+    }
+}

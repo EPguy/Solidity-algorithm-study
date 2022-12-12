5e7fea6b7fc9c021deb54a6014bebf840afca1d8
kjs <k021229@naver.com>
2022-12-12 09:50:38 +0900

    곱셈 문제

---
+++ MultiplicationCalculatorProblem.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+interface IMultiplicationCalculator {
+    function multiply(uint256, uint256) external pure returns (uint256);
+}
+
+contract MultiplicationCalculatorProblem{
+    IMultiplicationCalculator public multiplicationCalculator;
+
+    function setMultiplicationCalculator(address _multiplicationCalculator) public {
+        multiplicationCalculator = IMultiplicationCalculator(_multiplicationCalculator);
+    }
+}
+
+contract MyMultiplicationCalculator is IMultiplicationCalculator {
+    MultiplicationCalculatorProblem public multiplicationCalculatorProblem;
+
+    constructor(address _problemAddress) {
+        multiplicationCalculatorProblem = MultiplicationCalculatorProblem(_problemAddress);
+    }
+
+    function setCalculator() public{
+        multiplicationCalculatorProblem.setMinusCalculator(address(this));
+    }
+
+    function multiply(uint256 input1, uint256 input2) override public pure returns (uint256){
+        return input1 * input2;
+    }
+}

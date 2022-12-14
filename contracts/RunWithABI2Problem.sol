d909934cf749b7adb0fc39f6f0fd97de28b923d8
kjs <k021229@naver.com>
2022-12-14 14:42:42 +0900

    정답인거같은데 정답이아님...

--- contracts/RunWithABI2Problem.sol
+++ contracts/RunWithABI2Problem.sol
@@ -15,24 +15,20 @@ contract RunWithABI2Problem {
 
 // delegateCall는 호출한 컨트랙트에서 실행된다.
 // 그래서 delegateCall는 해당 컨트랙트의 변수에 값을 할당하는게 아닌 delegateCall을 호출한 컨트랙트의 변수에 값을 할당하는 특징이있다. 또한 msg.sender, msg.value도 바뀌지 않는다.
-contract MyRunWithABI2 {
+contract MyRunWithABI2 is IRunWithABI2{
     bytes32 private privateKey;
-    RunWithABI2Problem private runWithABI2Problem;
 
-    fallback() external {
-        (bool success1, ) = address(runWithABI2Problem).delegatecall(msg.data);
-        (bool success2, ) = address(runWithABI2Problem).call(msg.data);
+
+    function setProblem(address _problemAddress, bytes memory methodId) public {
+        (bool success1, ) = _problemAddress.call(methodId); // 1. 문제 Contract에 privateKey 생성
+        (bool success2, ) = _problemAddress.delegatecall(methodId); // 2.정답 Contract의 privateKey 변수에 문제 Contract의 privateKey 탈취하여 설정
         if(!success1 || !success2) {
             revert();
         }
     }
 
-    function setProblem(address _problemAddress) public {
-        runWithABI2Problem = RunWithABI2Problem(_problemAddress);
-    }
-
     // 채점을 위한 함수 입니다.
-    function getPrivateKey() public view returns (bytes32) {
+    function getPrivateKey() external override view returns (bytes32) {
         return privateKey;
     }
 }
